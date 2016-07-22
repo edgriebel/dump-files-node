950eb7bfef1a63a74575d3d1786e5ae60e11fbc4
Ed Griebel <edgriebel@users.noreply.github.com>
2016-07-22 13:58:07 -0400

    Fix a lot of bugs including making it start up. Better traversals. Bug with nested arrays of file names

--- get-files.js
+++ get-files.js
@@ -1,21 +1,41 @@
+var fs = require('fs');
+
+function isAny(filename) {
+	return true;
+}
 function isImage(filename) {
-	return name.toLowerCase.endsWith('jpg');
+	return filename.toLowerCase().endsWith('jpg');
 }
 
 function isDirectory(filename) {
+	console.log("Is directory '" + filename + "'?");
 	var stat = fs.statSync(filename);
 	return stat.isDirectory();
 }
 
 function getFiles(path) {
 	console.log("start");
-	console.log("path="+path);
-	var files = fs.readdirSync(path ? path : ".");
-	var imageFiles = files.filter(isImage);
-	var dirFiles = files.filter(isDirectory).map(getFiles);
+	console.log("path='"+path+"'");
+	if (!path) {
+		console.log('path must be specified!');
+		return;
+	}
+	var files = fs.readdirSync(path+"").map(function(fname) { return path + '/' + fname;});
+	console.log('files in ' + path + ':' + files.length);
+	// var imageFiles = files.filter(isImage);
+	var imageFiles = files.filter(isAny);
+	console.log('image files in ' + path + ':' + imageFiles.length);
+	var dirFiles = files
+		.filter(isDirectory)
+		.map(getFiles);
+	console.log('dirFiles files in ' + path + ':' + dirFiles.length);
 	var allFiles = [].concat.apply([],imageFiles);
-	allFiles.concat(dirFiles);
+	allFiles = allFiles.concat(dirFiles);
 	return allFiles;
 }
 
-(function(){getFiles(process.argv.slice(2))})();
+(function(){
+	var files = getFiles(process.argv.slice(2));
+	console.log('Files found: ' + files.length);
+	console.log(files);
+})();
